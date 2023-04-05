50e1081b1cb45fa5fd0492f1d037a3dba0a89da3
danielcastillopalma <d.castillo3@duocuc.cl>
2023-04-04 20:08:48 -0400

    se agrega swiper js

--- js/swiper.js
+++ js/swiper.js
@@ -1,8 +1,26 @@
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
-  spaceBetween: 30,
+  spaceBetween: 10,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  breakpoints: {
+    360: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
   },
 });
