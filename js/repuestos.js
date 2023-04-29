b82ffb18472ca2cbe489631990295bd35cda788e
danielcastillopalma <d.castillo3@duocuc.cl>
2023-04-28 20:15:00 -0400

    cambios generales

--- js/repuestos.js
+++ js/repuestos.js
@@ -7,18 +7,23 @@ function cargarProductos() {
     })
     .then(function (data) {
       let html = "";
-      data.forEach(function (producto) {
+      console.log(data);
+      data.some(function (producto) {
+        
         html += `
+        
           <div class="card" style="width: 18rem;">
             <img src=${producto.imgSrc} class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">${producto.nomProd}</h5>
               <p class="card-text">${producto.descProd}</p>
+              <h3>$${producto.valorProd}</h3>
             </div>
           </div>
+          
       `;
       document.getElementById('contenedorProdRep').innerHTML=html;
+      return producto.idProd===20;
       });
     });
 }
-cargarProductos();
