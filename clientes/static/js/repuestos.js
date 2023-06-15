d0b47f0f5ceac63f298d7ecc74c1a3c4edb2596d
danielcastillopalma <d.castillo3@duocuc.cl>
2023-06-15 17:49:10 -0400

    crud

---
+++ clientes/static/js/repuestos.js
@@ -0,0 +1,219 @@
+var posId = 1;
+var cont = 0;
+function cargarProductos() {
+  const e1 = document.querySelector(".p1");
+  e1.classList.add("activo");
+  const e2 = document.querySelector(".p2");
+  e2.classList.remove("activo");
+  const e3 = document.querySelector(".p3");
+  e3.classList.remove("activo");
+  let num = document.getElementById("up");
+  num.setAttribute("num", 1);
+  fetch(
+    "https://raw.githubusercontent.com/danielcastillopalma/api/master/db.json"
+  )
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      let html = "";
+      console.log(data);
+      data.some(function (producto) {
+        html += `
+        
+          <div class="card" style="width: 18rem;">
+            <img src=${producto.imgSrc} class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${producto.nomProd}</h5>
+              <p class="card-text">${producto.descProd}</p>
+              <h3>$${producto.valorProd}</h3>
+              <button class="btnMas csecundario clink" id="${producto.idProd}" onclick="agregar(this)"><i class="fa-duotone fa-plus fa-beat"></i></button>
+            </div>
+          </div>
+          
+      `;
+        document.getElementById("contenedorProdRep").innerHTML = html;
+        return producto.idProd === 20;
+      });
+    });
+}
+function page2() {
+  const e1 = document.querySelector(".p1");
+  e1.classList.remove("activo");
+  const e2 = document.querySelector(".p2");
+  e2.classList.add("activo");
+  const e3 = document.querySelector(".p3");
+  e3.classList.remove("activo");
+  fetch(
+    "https://raw.githubusercontent.com/danielcastillopalma/api/master/db.json"
+  )
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      let html = "";
+      console.log(data);
+      data.some(function (producto) {
+        if (producto.idProd > 20) {
+          html += `
+        
+          <div class="card" style="width: 18rem;">
+            <img src=${producto.imgSrc} class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${producto.nomProd}</h5>
+              <p class="card-text">${producto.descProd}</p>
+              <h3>$${producto.valorProd}</h3> 
+              <button class="btnMas cprimario  clink" id="${producto.idProd}" onclick="agregar(this)"><i class="fa-duotone fa-plus fa-beat"></i></button>
+            </div>
+          </div>
+          
+      `;
+          document.getElementById("contenedorProdRep").innerHTML = html;
+        }
+        return producto.idProd === 40;
+      });
+    });
+}
+function page3() {
+  const e1 = document.querySelector(".p1");
+  e1.classList.remove("activo");
+  const e3 = document.querySelector(".p3");
+  e3.classList.add("activo");
+  const e2 = document.querySelector(".p2");
+  e2.classList.remove("activo");
+
+  $('div').blur();
+
+  let num = document.getElementById("up");
+  num.setAttribute("num", 3);
+
+  fetch(
+    "https://raw.githubusercontent.com/danielcastillopalma/api/master/db.json"
+  )
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      let html = "";
+      console.log(data);
+      data.some(function (producto) {
+        if (producto.idProd > 40) {
+          html += `
+        
+          <div class="card" style="width: 18rem;">
+            <img src=${producto.imgSrc} class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${producto.nomProd}</h5>
+              <p class="card-text">${producto.descProd}</p>
+              <h3>$${producto.valorProd}</h3>
+              <button class="btnMas csecundario  clink" id="${producto.idProd}" onclick="agregar(this)"><i class="fa-duotone fa-plus fa-beat"></i></button>
+            </div>
+          </div>
+          
+      `;
+          document.getElementById("contenedorProdRep").innerHTML = html;
+        }
+        var posId = producto.idProd;
+        return producto.idProd === 60;
+      });
+    });
+}
+
+function pagUp() {
+  let num = document.getElementById("up");
+  let actual = num.getAttribute("num");
+  num.setAttribute("num", parseInt(actual) + 1);
+  let sig = num.getAttribute("num");
+
+  fetch(
+    "https://raw.githubusercontent.com/danielcastillopalma/api/master/db.json"
+  )
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      let html = "";
+      console.log(data);
+      data.some(function (producto) {
+        if (producto.idProd > actual * 20) {
+          html += `
+        
+          <div class="card" style="width: 18rem;">
+            <img src=${producto.imgSrc} class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${producto.nomProd}</h5>
+              <p class="card-text">${producto.descProd}</p>
+              <h3>$${producto.valorProd}</h3>
+              <button class="btnMas csecundario  clink" id="${producto.idProd}" onclick="agregar(this)"><i class="fa-duotone fa-plus fa-beat"></i></button>
+            </div>
+          </div>
+          
+      `;
+          document.getElementById("contenedorProdRep").innerHTML = html;
+        }
+        return producto.idProd === sig * 20;
+      });
+    });
+}
+function pagDown() {
+  let num = document.getElementById("up");
+  let actual = num.getAttribute("num");
+  num.setAttribute("num", parseInt(actual) - 1);
+  let nueva = num.getAttribute("num");
+
+  fetch(
+    "https://raw.githubusercontent.com/danielcastillopalma/api/master/db.json"
+  )
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      let html = "";
+      console.log(data);
+      data.some(function (producto) {
+        if (producto.idProd > (nueva - 1) * 20 && actual > 1) {
+          html += `
+        
+          <div class="card" style="width: 18rem;">
+            <img src=${producto.imgSrc} class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${producto.nomProd}</h5>
+              <p class="card-text">${producto.descProd}</p>
+              <h3>$${producto.valorProd}</h3>
+              <button class="btnMas csecundario  clink" id="${producto.idProd}" onclick="agregar(this)"><i class="fa-duotone fa-plus fa-beat"></i></button>
+            </div>
+          </div>
+          
+      `;
+          document.getElementById("contenedorProdRep").innerHTML = html;
+        }
+        var posId = producto.idProd;
+        return producto.idProd === nueva * 20;
+      });
+    });
+}
+
+/*
+arreglar codigo de abajo
+*/
+
+function agregar(produId) {
+  let id = produId.id;
+  console.log(id);
+
+  fetch(
+    "https://raw.githubusercontent.com/danielcastillopalma/api/master/db.json"
+  )
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      for (var prodId in data) {
+        console.log(' name=' + prodId + ' value=' + data[id]);
+        if(prodId===id){
+          return;
+        }
+     }
+    });
+
+}
--- clientes/urls.py
+++ clientes/urls.py
@@ -6,5 +6,8 @@ from . import views
 
 urlpatterns =[
     path('index',views.index,name='index'),
-    
+    path('customizacion',views.customizacion,name='customizacion'),
+    path('login',views.login,name='login'), 
+    path('repuestos',views.repuestos,name='repuestos'),
+    path('servicios',views.servicios,name='servicios')
 ]
