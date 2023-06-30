fc0045794d085fd1ad013ae33b5f35e7c3e09bd2
danielcastillopalma <d.castillo3@duocuc.cl>
2023-06-29 21:09:50 -0400

    git

--- clientes/urls.py
+++ clientes/urls.py
@@ -5,12 +5,12 @@ from . import views
 
 
 urlpatterns =[
-    path('index.html',views.index,name='index'),
-    path('customizacion.html',views.customizacion,name='customizacion'),
-    path('login.html',views.login,name='login'), 
-    path('repuestos.html',views.repuestos,name='repuestos'),
-    path('servicios.html',views.servicios,name='servicios'),
-    path('register.html',views.register, name='register'),
+    path('index',views.index,name='index'),
+    path('customizacion',views.customizacion,name='customizacion'),
+    path('login',views.login,name='login'), 
+    path('repuestos',views.repuestos,name='repuestos'),
+    path('servicios',views.servicios,name='servicios'),
+    path('register',views.register, name='register'),
     path('crud', views.crud, name='crud'),
     path('clientesAdd', views.clientesAdd, name='clientesAdd'),
     
